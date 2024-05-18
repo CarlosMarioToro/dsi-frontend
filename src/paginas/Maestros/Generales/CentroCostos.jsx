bf87b463066cc5473fe9f4512a0a141bf2a60dd8
CarlosMarioToro <ctoroz59@gmail.com>
2024-05-18 17:42:33 -0500

    Ajustes a formularios

--- src/paginas/Maestros/Generales/CentroCostos.jsx
+++ src/paginas/Maestros/Generales/CentroCostos.jsx
@@ -1,54 +1,79 @@
-import { DataGrid } from '@mui/x-data-grid';
-import clienteAxios from '../../../config/axios';
-import Alerta from '../../../componentes/Alerta';
-import React, { useState } from 'react';
+import { DataGrid } from "@mui/x-data-grid";
+import clienteAxios from "../../../config/axios";
+import Alerta from "../../../componentes/Alerta";
+import React, { useState } from "react";
 
 const columns = [
-    { field: "id", headerName: "Código", width: 80,headerClassName: 'super-app-theme--header',
-    headerAlign: 'center' },
-    { field: "col1", headerName: "Nombre", width: 150,headerClassName: 'super-app-theme--header',
-    headerAlign: 'center' },
-    { field: "col2", headerName: "Cod Contable", width: 150,headerClassName: 'super-app-theme--header',
-    headerAlign: 'center' },
+  {
+    field: "id",
+    headerName: "Código",
+    width: 80,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "col1",
+    headerName: "Nombre",
+    width: 150,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "col2",
+    headerName: "Cod Contable",
+    width: 150,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
 ];
 
 let rows = [];
 
 const CentroCostos = () => {
-    const [alerta, setAlerta] = useState({});
+  const [alerta, setAlerta] = useState({});
 
-    React.useEffect(() => {
-        async function obtenerCentroCostos() {
-            const { data } = await clienteAxios.get("/centro-costos");
-            rows = [];
-            for(var i in data) {
-                let centroCostos = {
-                    id: data[i].ccc_codigo,
-                    col1: data[i].ccc_nombre,
-                    col2: data[i].ccc_cod_contable
-                }
-                rows = [...rows, centroCostos];
-            }
-            setAlerta({ msg: data.msg });
-        }
-        obtenerCentroCostos();
-    }, []);
+  React.useEffect(() => {
+    async function obtenerCentroCostos() {
+      const { data } = await clienteAxios.get("/centro-costos");
+      rows = [];
+      for (var i in data) {
+        let centroCostos = {
+          id: data[i].ccc_codigo,
+          col1: data[i].ccc_nombre,
+          col2: data[i].ccc_cod_contable,
+        };
+        rows = [...rows, centroCostos];
+      }
+      setAlerta({ msg: data.msg });
+    }
+    obtenerCentroCostos();
+  }, []);
 
-    const { msg } = alerta;
+  const { msg } = alerta;
 
-    return (
-        <>
-            <section className="container w-full h-full items-center">
-                <div className="flex flex-col items-center h-full w-auto bg-gray-300 rounded-lg p-4 bg-opacity-75">
-                    <h1 className="w-full p-3 bg-gray-500 rounded-t-lg">Maestro de Centro de Costos</h1>
-                    {msg && <Alerta alerta={alerta} />}
-                    <div className="w-auto h-full bg-white bg-opacity-75">
-                        <DataGrid columns={columns} rows={rows} />
-                    </div>                    
-                </div>                
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="container w-full h-full items-center">
+        <div className="flex flex-col items-center h-full w-auto bg-gray-300 rounded-lg p-4 bg-opacity-75">
+          <h1 className="w-full p-3 bg-gray-500 rounded-t-lg">
+            Maestro de Centro de Costos
+          </h1>
+          {msg && <Alerta alerta={alerta} />}
+          <div className="w-auto h-full bg-white bg-opacity-75">
+            <DataGrid
+              columns={columns}
+              rows={rows}
+              rowHeight={25}
+              autoPageSize
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default CentroCostos;
--- src/paginas/Maestros/Generales/Ciudades.jsx
+++ src/paginas/Maestros/Generales/Ciudades.jsx
@@ -1,29 +1,88 @@
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid } from "@mui/x-data-grid";
+import clienteAxios from "../../../config/axios";
+import Alerta from "../../../componentes/Alerta";
+import React, { useState } from "react";
 
 const columns = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
+  {
+    field: "id",
+    headerName: "Código",
+    width: 80,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "col1",
+    headerName: "Nombre",
+    width: 150,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "col2",
+    headerName: "Cod Dian",
+    width: 150,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "col3",
+    headerName: "Departamento",
+    width: 150,
+    headerClassName: "super-app-theme--header",
+    headerAlign: "center",
+    align: "center",
+  },
 ];
 
-const rows = [
-  { id: 1, col1: 'Hello', col2: 'World ' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
+let rows = [];
 
 const Ciudades = () => {
+  const [alerta, setAlerta] = useState({});
+
+  React.useEffect(() => {
+    async function obtenerCiudades() {
+      const { data } = await clienteAxios.get("/ciudades");
+      rows = [];
+      for (var i in data) {
+        let ciudades = {
+          id: data[i].ciu_codigo,
+          col1: data[i].ciu_nombre,
+          col2: data[i].ciu_cod_dian,
+          col3: data[i].dpt_nombre,
+        };
+        rows = [...rows, ciudades];
+      }
+      setAlerta({ msg: data.msg });
+    }
+    obtenerCiudades();
+  }, []);
+
+  const { msg } = alerta;
+
   return (
     <>
-        <section className="container w-full h-full items-center">
-            <div className="flex flex-col items-center h-full w-auto bg-gray-300 rounded-lg p-4 bg-opacity-75">
-                <h1 className="w-full p-3 bg-gray-500 rounded-t-lg">Maestro de Ciudades</h1>
-                <div className="w-auto h-full bg-white bg-opacity-75">
-                    <DataGrid columns={columns} rows={rows} />
-                </div>                    
-            </div>            
-        </section>
+      <section className="container w-full h-full items-center">
+        <div className="flex flex-col items-center h-full w-auto bg-gray-300 rounded-lg p-4 bg-opacity-75">
+          <h1 className="w-full p-3 bg-gray-500 rounded-t-lg">
+            Maestro de Ciudades
+          </h1>
+          {msg && <Alerta alerta={alerta} />}
+          <div className="w-auto h-full bg-white bg-opacity-75">
+            <DataGrid
+              columns={columns}
+              rows={rows}
+              rowHeight={25}
+              autoPageSize
+            />
+          </div>
+        </div>
+      </section>
     </>
-);
-}
+  );
+};
 
-export default Ciudades
+export default Ciudades;
